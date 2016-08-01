0082b6c60a495f223eb254ce1baaa17e33e8b571
Deepak275 <deeepak1300@gmail.com>
2016-08-01 12:35:00 +0530

    Revert "Define the variable type output in the test/branches.js"
    This reverts commit 492426b1f5bf0b7f31386487d53b48ad0a91b861.

--- test/branches.js
+++ test/branches.js
@@ -6,7 +6,7 @@ describe('inMemory', function () {
     let p = new Parser();
     branches = p.parse("test/ifsc.xls");
     console.log(branches);
-    let output = branches.find("400065003");
+    output = branches.find("400065003" );
     expect(output.length).to.be.equal(1);
   });
 });
